f934b32b922713db6e80354d3fde8f656060b9ed
unknown <yeasin.mohammod018@gmail.com>
2021-08-10 12:32:00 +0600

    drawing the clock

--- myScript.js
+++ myScript.js
@@ -1,2 +1,13 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d"); // Here 'ctx' indicates 'context'
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+
+radius = radius * 0.90;
+drawClock();
+
+function drawClock() {
+    ctx.arc(0,0,radius,0,2*Math.PI);
+    ctx.fillStyle = "White";
+    ctx.fill();
+}
